9d445ab5bb319dde86e1f87a15c1e5ffd6b273a0
Christian Rudh <christian@rudh.se>
2014-01-12 22:09:33 +0100

    Cleaned up the code and made it simpler.

--- output.js
+++ output.js
@@ -2,19 +2,14 @@ define(function (require) {
     'use strict';
 
     var output = {
-        cleanPath: function (path, outputDir) {
-            while (path.indexOf(outputDir) == 0) {
-                path = path.substring(outputDir.length + 1)
-            }
-
-            return path;
-        },
-
         load: function (name, req, onload, config) {
             var outputDir = config.config.output.outputDir;
-            var cleanPath = this.cleanPath(name, outputDir);
 
-            req([outputDir + '/' + cleanPath], function (value) {
+            while (name.indexOf(outputDir) == 0) {
+                name = name.substring(outputDir.length + 1);
+            }
+
+            req([outputDir + '/' + name], function (value) {
                 onload(value);
             });
         }
